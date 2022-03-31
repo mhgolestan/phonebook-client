4bd31c67cb9e4900b393b80e3ac16ac19885b1e2
mhgolestan <golestan1369@gmail.com>
2022-03-31 12:09:46 +0200

    2.8: The Phonebook Step3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "mohammad" }]);
-  const [newName, setNewName] = useState("Enter name");
+  const [persons, setPersons] = useState([
+    { name: "mohammad", phone: 1234, id: 1 },
+  ]);
+  const [newName, setNewName] = useState({});
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const nameObject = {
-      name: newName,
+      name: newName.name,
+      phone: newName.phone,
+      id: persons.length + 1,
     };
+
     if (persons.find((person) => person.name === nameObject.name)) {
       alert(`${nameObject.name} is already added to phonebook`);
     } else {
@@ -17,8 +22,10 @@ const App = () => {
     }
   };
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setNewName((values) => ({ ...values, [name]: value }));
   };
 
   return (
@@ -27,13 +34,30 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Enter name:{" "}
-          <input type="text" value={newName} onChange={handleNameChange} />
+          <input
+            type="text"
+            name="name"
+            value={newName.name || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Enter phone number: {""}
+          number:{" "}
+          <input
+            type="number"
+            name="phone"
+            value={newName.phone || ""}
+            onChange={handleChange}
+          />
         </label>
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <li key={person.name}>{person.name}</li>
+        <li key={person.id}>
+          {person.name} {person.phone}
+        </li>
       ))}
     </div>
   );
